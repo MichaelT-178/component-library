d033195091bd81773074aea8b80b2f8680710fbb
Michael Totaro <mstotaro@gmail.com>
2024-11-14 17:46:18 -0500

    New Components

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,28 @@
 import vue from "rollup-plugin-vue";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
-  output: {
-    format: "es",
-    file: "dist/index.js",
-  },
-  plugins: [vue(), resolve(), commonjs()],
+  output: [
+    {
+      format: "es",
+      file: "dist/index.esm.js",
+    },
+    {
+      format: "cjs",
+      file: "dist/index.cjs.js",
+      exports: "named",
+    },
+  ],
+  plugins: [
+    vue(),
+    postcss(),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
   external: ["vue"],
 };
